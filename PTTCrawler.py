7d95804ceb1e2431fcabcaee910963e574c75b15
Tzu-Li Tai <tzulitai@ee.ncku.edu.tw>
2014-07-15 12:50:47 +0800

    Add PTTCrawler class (for OOP rearrangement)
    - methods: __init__(boardName, startIndex, endIndex), crawl()

---
+++ PTTCrawler.py
@@ -0,0 +1,48 @@
+from HTMLCommons import HTMLCommons
+from PTTCommons import PTTCommons
+
+class PTTCRawler:
+	
+	cookies = {"over18": "1"}
+	boardName = ""
+	startIndex = 0
+	endIndex = 0
+	outerIndexURL = "http://www.ptt.cc/bbs/{board}/index{indexNo}.html"
+	innerPostURL = "http://www.ptt.cc{postURL}"
+
+	def __init__(self,boardName,sIndx, eIndx):
+		self.boardName = boardName
+		self.startIndex = sIndx
+		self.endIndex = eIndx
+
+	def crawl(self):
+		currentCrawledIndex = sIndx
+		postID = 0
+		postIDString = ""
+
+		for i in range(eIndx-sIndx+1):
+
+			postIDStringPrefix = self.BoardName + "_" + str(currentCrawledIndex)
+
+			# 1st: the URL to establish connection with
+			# 2nd: cookies
+			response = requests.get(
+				PTTCrawler.outerIndexURL.format(board = self.boardName, indexNo = str(currentCrawledIndex)),
+				PTTCrawler.cookies
+				)
+			soup = BeautifulSoup(response.text)
+			
+			for tag in soup.find_all(HTMLCommons.DIVISION,PTTCommons.HtmlClassTags.POST):
+				try:
+					link = str(tag.find_all(HTMLCommons.HYPERLINK))
+					link = link.split("\"")
+					link = PTTCrawler.innerPostURL.format(postURL = link[1])
+
+					postID += 1
+					postIDString = postIdStringPrefix + "_" + postID
+					parsePost(link,postIDString)
+				except:
+					pass
+
+			sleep(0.2)
+			currentCrawledIndex += 1
