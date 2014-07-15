5f1f2961b7701a32b0867de69caf4944bee6d799
Tzu-Li Tai <tzulitai@ee.ncku.edu.tw>
2014-07-15 16:49:37 +0800

    Updated some global constants that are needed

--- PTTCommons.py
+++ PTTCommons.py
@@ -1,9 +1,15 @@
 class PTTCommons:
 	
 	class HtmlClassTags:
-		POST = "r-ent"
+		OUTER_POST = "r-ent"
+		CONTENT_CONTAINER = "main-container"
+		PUSH = "push"
+		PUSH_TAG = "push-tag"
+		PUSH_USERID = "push-userid"
+		PUSH_CONTENT = "push-content"
+		PUSH_TIME = "push-ipdatetime"
+		POST_SOURCE_MSG = "※ 發信站:"	# used for source IP parsing
 
-	class PostTags:
-
-
-	class MsgCommons:
+	class PushTagsCH:
+		LIKE = "推"
+		NLIKE = "噓"
