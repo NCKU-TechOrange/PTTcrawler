bd73c564a230ee1d214331f3bade1a87ad0dbabc
Tzu-Li Tai <tzulitai@ee.ncku.edu.tw>
2014-07-15 16:50:54 +0800

    Finalized initial OOP-version of PTTCrawler

--- PTTCrawler.py
+++ PTTCrawler.py
@@ -1,9 +1,9 @@
 from HTMLCommons import HTMLCommons
 from PTTCommons import PTTCommons
 
-class PTTCRawler:
+class PTTCrawler:
 	
-	cookies = {"over18": "1"}
+	COOKIES = {"over18": "1"}
 	boardName = ""
 	startIndex = 0
 	endIndex = 0
@@ -28,11 +28,11 @@ class PTTCRawler:
 			# 2nd: cookies
 			response = requests.get(
 				PTTCrawler.outerIndexURL.format(board = self.boardName, indexNo = str(currentCrawledIndex)),
-				PTTCrawler.cookies
+				PTTCrawler.COOKIES
 				)
 			soup = BeautifulSoup(response.text)
 			
-			for tag in soup.find_all(HTMLCommons.DIVISION,PTTCommons.HtmlClassTags.POST):
+			for tag in soup.find_all(HTMLCommons.DIVISION,PTTCommons.HtmlClassTags.OUTER_POST):
 				try:
 					link = str(tag.find_all(HTMLCommons.HYPERLINK))
 					link = link.split("\"")
@@ -40,9 +40,20 @@ class PTTCRawler:
 
 					postID += 1
 					postIDString = postIdStringPrefix + "_" + postID
-					parsePost(link,postIDString)
+					__parsePost(link,postIDString)
 				except:
 					pass
 
 			sleep(0.2)
 			currentCrawledIndex += 1
+
+	def __parsePost(self,postIDString):
+		response = requests.get(link,PTTCrawler.COOKIES)
+		soupHtmlParser = BeautifulSoup(response.text)
+
+		postToParse = PTTPost(postIDString)
+		postParser = PTTPostParser(postToParse, soupHtmlParser)
+		
+		postParser.parse()
+		
+		postParser.storeAsJSON("data2.json") 
