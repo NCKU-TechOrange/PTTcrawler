f1e7da3c1cc061a28edb77665f4fd800045ab00b
Tzu-Li Tai <tzulitai@ee.ncku.edu.tw>
2014-07-15 16:54:27 +0800

    Add class PTTPost. The object class for a single PTT post.

---
+++ PTTPost.py
@@ -0,0 +1,25 @@
+class PTTPost:
+	
+	def __init__(self,postIDStr):
+		self.POST_ID = postIDStr
+
+	def setAuthor(self,author):
+		self.AUTHOR = author
+
+	def setTitle(self,title):
+		self.TITLE = title
+
+	def setDate(self,date):
+		self.DATE = date
+
+	def setIP(self,ipAddr):
+		self.IP = ipAddr
+
+	def setContent(self,content):
+		self.CONTENT = content
+
+	def setMsg(self,message):
+		self.MSG = message
+
+	def setMsgCountInfo(self,msgCountInfo)
+		self.MSG_COUNT_INFO = msgCountInfo
