5d8fc5c94cc12213dc80b9d5fc98a0666f8aa1a6
Tzu-Li Tai <tzulitai@ee.ncku.edu.tw>
2014-07-18 00:26:46 +0800

    Allowed user to specify output JSON path via main system argument

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-import re
 import sys
-import json
 import requests
-from time import sleep
-from bs4 import BeautifulSoup  
 from PTTCrawler import PTTCrawler
 
 
 boardName = str(sys.argv[1])
 startIndex = int(sys.argv[2])
 endIndex = int(sys.argv[3])
+outputPath = str(sys.argv[4])
 
 crawler = PTTCrawler(boardName, startIndex, endIndex)
-crawler.crawl()
+crawler.crawl(outputPath)
